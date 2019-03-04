fd16446f9ad36a3de7f077cdb159e11d6a2d2ac2
liudi <liudi10@meituan.com>
2019-03-04 14:40:39 +0800

    Feat: done

--- src/index.js
+++ src/index.js
@@ -5,19 +5,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
 import "semantic-ui-css/semantic.min.css";
-import StylishCalligraphyDemo from "./assets/fonts/StylishCalligraphyDemo.woff";
-import EauSansBook from "./assets/fonts/eau_sans_book.woff";
-import EauSansBold from "./assets/fonts/eau_sans_bold.woff";
+
+import "./assets/fonts/StylishCalligraphyDemo.woff";
+import "./assets/fonts/eau_sans_book.woff";
+import "./assets/fonts/eau_sans_bold.woff";
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
     document.getElementById("root")
   );
 
